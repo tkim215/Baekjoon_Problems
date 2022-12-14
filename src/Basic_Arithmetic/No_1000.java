67fdd730cdeee5200cc552f2ef1526a30baf3f94
tkim215 <taeyunkim0215@gmail.com>
2022-12-14 19:03:47 +0900

    commit prob no.1000

---
+++ src/Basic_Arithmetic/No_1000.java
@@ -0,0 +1,16 @@
+package Basic_Arithmetic;
+
+import java.util.*;
+
+public class No_1000 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+
+		System.out.println(A + B);
+		
+		scanner.close();
+	}
+}
